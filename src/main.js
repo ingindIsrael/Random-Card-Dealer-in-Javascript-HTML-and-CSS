5c7481d9a0106cf94834d7dee2017b96393b4248
Israel Vasquez <ing.visrael@gmail.com>
2021-02-16 18:47:54 +0000

    cards

--- src/main.js
+++ src/main.js
@@ -6,5 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  loadCards();
 };
+function loadCards() {
+  var RN = Math.floor(Math.random() * 13);
+  var RN2 = Math.floor(Math.random() * 4);
+  var simbolos = ["\u2665", "\u2666", "\u2663", "\u2660"];
+  var numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "A",
+    "J"
+  ];
+  var numero = document.getElementById("Nro");
+  numero.innerHTML = numbers[RN];
+
+  var sim = document.querySelectorAll(".simbolos");
+  //sim.innerHTML = simbolos[RN2];
+  for (var i = 0; i < sim.length; i++) {
+    if (RN % 2 == 0) {
+      sim[0].style.color = "red";
+      sim[1].style.color = "red";
+    }
+    sim[i].innerHTML = simbolos[RN2];
+  }
+}
